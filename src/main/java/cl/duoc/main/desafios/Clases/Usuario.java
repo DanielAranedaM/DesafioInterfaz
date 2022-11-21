0a2426ef887d7a9640db24a4de04e39d48a05beb
DanielAranedaM <d.aranedam@duocuc.cl>
2022-11-21 14:01:15 -0300

    agrege desafio

---
+++ src/main/java/cl/duoc/main/desafios/Clases/Usuario.java
@@ -0,0 +1,30 @@
+
+package cl.duoc.main.desafios.Clases;
+
+public class Usuario {
+    private String user;
+    private String contrasena;
+
+    public Usuario() {
+        this.user = "";
+        this.contrasena ="" ;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
